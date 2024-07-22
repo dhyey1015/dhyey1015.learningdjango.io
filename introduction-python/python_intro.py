a95100df8f8ccd1a355ec85a0e6b898bc54d0192
Dhyey <dhyeykakadiya1015@gmail.com>
2024-07-22 16:47:34 +0530

    added elif

--- introduction-python/python_intro.py
+++ introduction-python/python_intro.py
@@ -5,4 +5,16 @@ if 5>2:
     print("5 is indeed greater than 2")
 else:
     print("5 is not greater than 2")
+
+name = "xyz"
+
+if name == "abc":
+    print("hello, abc")
+    
+elif name == "xyz":
+    print("hello, xyz")
+    
+else:
+    print("hey anonymous")
+    
     
