fbe7abe44bb602d97a8d2eaa3d6e2d8cc0b81c63
Dhyey <dhyeykakadiya1015@gmail.com>
2024-07-22 17:41:04 +0530

    added loops program

---
+++ introduction-python/loops.py
@@ -0,0 +1,14 @@
+
+#lopp-0-----------------------------------------------------------
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+
+def hi(name):
+    print("hi there", name)
+    
+for name in girls:
+    hi(name)
+    print("Next girl!!!")
+    
+#loop-1---------------------------------------------------------
+for i in range(1, 6):
+    print(i)
