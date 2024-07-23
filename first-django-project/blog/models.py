468a50cf57cd4fb3fcefc2d047e5923e3ed30c1f
Dhyey <dhyeykakadiya1015@gmail.com>
2024-07-23 12:41:26 +0530

    fixed some errors related to models added

--- first-django-project/blog/models.py
+++ first-django-project/blog/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.utils import timezone
 
 class Post(models.Model):
-    auther = models.ForeginKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE) 
+    auther = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE) 
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
